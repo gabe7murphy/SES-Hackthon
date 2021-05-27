8bb1fd21861e8ef9abaadbd4ab6772115d7e2097
Sarah Gu <guwen888@gmail.com>
2021-05-27 18:13:34 -0400

    automatic data push

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
+//elastic api packages
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const elastic = require('elasticsearch');
+const bodyParser = require('body-parser').json();
+const elasticClient = elastic.Client({
+    host: 'localhost:5601',
+});
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Q3Uzd3Jua0JXZkFhN3lpc2xMMkw6emxWOGNhZUFUSXU4eW5zaEkzVnF3QQ'
+}
+
+
 // Initialize butotn with users's prefered color
 let submit = document.getElementById("submit-btn");
 let form = document.getElementById("donationForm");
@@ -29,6 +44,8 @@ function addDonation(donation) {
   });
 
 
+
+
     // chrome.storage.sync.get(["donations"], function(result) {
     //   var array = result[storagekey]?result[storagekey]:[];
 
@@ -45,7 +62,17 @@ function addDonation(donation) {
     //   console.log('Value is set to ' + value);
     // });
 }
+function send_elasticAPI(data){
+  axios.post('http://localhost:9200/donations2/_doc',
+      data, { headers: headers }
+    ).then((response) => {
+      console.log(response)
+   })
+    .catch((error) => {
+          console.log(error)
+    })
 
+}
 function documentEvents() {    
   submit.addEventListener('click', () => { 
     
@@ -71,6 +98,7 @@ function documentEvents() {
       charity_input.value = "";
       amount_input.value = "";
       addDonation(donation);
+      send_elasticAPI(donation); 
   });
 
   // you can add listeners for other objects ( like other buttons ) here 
