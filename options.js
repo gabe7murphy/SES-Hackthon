cdb71e1887832bb582b8c509a0a1b84b6070644c
gabe7murphy <gabe7murphy@gmail.com>
2021-05-27 14:52:31 -0400

    something works

--- options.js
+++ options.js
@@ -1,10 +1,35 @@
 const express = require('express');
-const router = express.Router(); 
-const elastic = require('elasticsearch'); 
-const bodyParser = require('body-parser').json(); 
+const router = express.Router();
+const axios = require('axios');
+const elastic = require('elasticsearch');
+const bodyParser = require('body-parser').json();
 const elasticClient = elastic.Client({
     host: 'localhost:5601',
 });
+
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Q3Uzd3Jua0JXZkFhN3lpc2xMMkw6emxWOGNhZUFUSXU4eW5zaEkzVnF3QQ'
+}
+const data = {
+    charity_name: "doctors w/o borders",
+    amount: 500,
+    tag: "health",
+    date: "5/03/2021"
+}
+axios.post('http://localhost:9200/donations2/_doc',
+
+    data, { headers: headers }
+).then((response) => {
+    console.log(response)
+})
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
+
 /*
     elasticClient.index({
         index: 'logs', 
@@ -22,12 +47,15 @@ const elasticClient = elastic.Client({
     })
     next(); 
 */
-    elasticClient.index({
-        index: 'donations2', 
-        body: {
-            charity_name: "doctors w/o borders", 
-            amount: 500,
-            tag: "health", 
-            date: "5/03/2021"
-        }
-    })
+
+/*
+elasticClient.index({
+    index: 'donations2',
+    body: {
+        charity_name: "doctors w/o borders",
+        amount: 500,
+        tag: "health",
+        date: "5/03/2021"
+    }
+})
+*/
